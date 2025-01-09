47f536e91c8d5a4d823a8eca89e6dd8f0d47adba
burak <devx.burak@gmail.com>
2025-01-09 20:07:28 +0300

    Initial Commit

---
+++ Pomodoro Timer/Pomodoro_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_TimerApp.swift
+//  Pomodoro Timer
+//
+//  Created by Burak on 9.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
